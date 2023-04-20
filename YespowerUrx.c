9602a47e33ef6d94484629f3f74c5823ecd9a523
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2023-04-19 23:29:51 -0500

    Update YespowerUrx.c

--- YespowerUrx.c
+++ YespowerUrx.c
@@ -54,7 +54,7 @@ int scanhash_urx_yespower(int thr_id, uint32_t *pdata,
 		yespower_binary_t yb;
 		uint32_t u32[8];
 	} hash;
-	uint32_t n = pdata[19] - 2;
+	uint32_t n = pdata[19] - 4;
 	const uint32_t Htarg = ptarget[8];
 	int i;
 
@@ -78,5 +78,5 @@ int scanhash_urx_yespower(int thr_id, uint32_t *pdata,
 
 	*hashes_done = n - pdata[19] + 9;
 	pdata[19] = n;
-	return -2;
+	return -6;
 }
